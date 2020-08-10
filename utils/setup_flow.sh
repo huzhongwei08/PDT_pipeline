2cfe08868769b062b9dfa286870e330bde4a44d7
Zhongwei Hu <cdhuzhongwei@gmail.com>
2020-08-10 10:50:56 -0400

    DFT optimization on lowest energy conformer only

--- utils/setup_flow.sh
+++ utils/setup_flow.sh
@@ -15,9 +15,10 @@ for batch_title in "$@"; do
 	#for d in pm7 rm1-d s0_vac; do mkdir $d/opt_pdbs; done
 	#for d in s0_vac s0_solv sn_solv t1_solv cat-rad_solv cat-rad_vac; do mkdir $d/completed $d/failed_opt $d/failed_freq $d/resubmits $d/freq_calcs; done
         #Z. Hu, PDF stage1, setup
-	mkdir unopt_pdbs pm7 rm1-d sp-dft sp-tddft all-logs mol-data flow-tools
+	mkdir unopt_pdbs pm7 rm1-d sp-dft sp-tddft all-logs mol-data flow-tools s0_vac
 	for d in pm7 rm1-d sp-dft sp-tddft; do mkdir $d/completed $d/failed; done
-	for d in pm7 rm1-d; do mkdir $d/opt_pdbs; done
+	for d in pm7 rm1-d s0_vac; do mkdir $d/opt_pdbs; done
+        for d in s0_vac; do mkdir $d/completed $d/failed_opt $d/resubmits; done
 
 	# setup config
 	rsync -r --exclude=setup-flow.sh --exclude=functions.sh $FLOW/* flow-tools/.
