4f496aa67650032cde4b67f78067cc22e864294e
Zhongwei Hu <cdhuzhongwei@gmail.com>
2020-12-12 14:44:00 -0500

    Two XC functionals for TDDFT

--- utils/setup_flow.sh
+++ utils/setup_flow.sh
@@ -16,9 +16,12 @@ for batch_title in "$@"; do
 	#for d in s0_vac s0_solv sn_solv t1_solv cat-rad_solv cat-rad_vac; do mkdir $d/completed $d/failed_opt $d/failed_freq $d/resubmits $d/freq_calcs; done
         #Z. Hu, PDF stage1, setup
 	mkdir unopt_pdbs pm7 rm1-d sp-dft sp-tddft all-logs mol-data flow-tools s0_vac soc
-	for d in pm7 rm1-d sp-dft sp-tddft; do mkdir $d/completed $d/failed; done
+	for d in pm7 rm1-d sp-dft; do mkdir $d/completed $d/failed; done
 	for d in pm7 rm1-d s0_vac; do mkdir $d/opt_pdbs; done
         for d in s0_vac; do mkdir $d/completed $d/failed_opt $d/resubmits; done
+        for d in sp-tddft; do mkdir $d/b3lyp $d/wb97xd; done;
+        for d in sp-tddft; do mkdir $d/b3lyp/completed $d/b3lyp/failed $d/wb97xd/completed $d/wb97xd/failed; done
+        for d in soc; do mkdir $d/b3lyp $d/wb97xd; done;
 
 	# setup config
 	rsync -r --exclude=setup-flow.sh --exclude=functions.sh $FLOW/* flow-tools/.
